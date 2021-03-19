876527787b74b350696638bb43248a63b1e17612
hyunwoo park <haram0407@naver.com>
2021-03-19 10:04:21 +0900

    수정

---
+++ 기본.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+from PyPDF2 import PdfFileReader # pdf파일 읽어오는 라이브러리
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg') #서버에서 화면에 표시하기 위해서 필요
+import seaborn as sns
+
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()
